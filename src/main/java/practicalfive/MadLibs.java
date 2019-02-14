364d196a9dcde3c4e7417a8329dd8782352e4ff0
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-02-14 15:53:35 -0500

    Starter files for practical 5

---
+++ src/main/java/practicalfive/MadLibs.java
@@ -0,0 +1,40 @@
+package practicalfive;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Date;
+import java.util.Scanner;
+
+/** This class has a method that creates a Mid Libs story.
+ * The program utilizes the words in a text file.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Janyl Jumadinova
+ */
+
+public class MadLibs {
+
+  /** The main entry point for MadLibs.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window.
+   *
+   **/
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Janyl Jumadinova " + new Date());
+    // declare the starting file and scanner
+    Scanner scanner = new Scanner(System.in);
+    // Step Zero: Modify the statement printed below to fit your needs
+    System.out.println("Please enter ... ");
+          
+    // Step One: Read in and save the input from the user
+
+    // Step Two: Display the words and numbers in a Mad Libs story
+
+    // Step Three: Display a final thank you message
+
+  }
+
+}
